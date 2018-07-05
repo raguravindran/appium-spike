fd79d71a688aa018c4b121c048cd7b48cb0f9a54
rsssa02 <rsssa02@ca.com>
2018-07-05 13:09:39 +0530

    merging new changes with report integration

--- appium.protractor.conf.js
+++ appium.protractor.conf.js
@@ -1,79 +1,120 @@
 'use strict';
-var basePath = './src/test/system/common',
-    argv = require('minimist')(process.argv.slice(2)),
-    deviceName = "emulator-5554",
-    app = "c:\\sw\\app-debug.apk",
-    appCloud = "PRIVATE:",
-    platformVersion = "8.1.1",
+var Dlist = require('./devices.json'),
+    fs = require('fs'),
+    path = require('path'),
+    reporters = require('jasmine-reporters'),
+    specReporter = require('jasmine-spec-reporter').SpecReporter,
+    allureReporter = require('jasmine-allure-reporter'),
+    jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter'),
     url = "http://localhost:4723/wd/hub";
-const user = 'rsssa02@ca.com';
-const pass = 'P@ssw0rd';
+var multiCapabilities = [];
 
-if(argv.perfecto) {
-    if(argv.perfecto === true) {
-        if(!argv.deviceName || argv.deviceName === null || !argv.app || argv.app === null) {
-            console.info('== Usage: --perfecto true --deviceName emulator-5554 --app app-debug.apk ==');
-            process.exit(1);
-        } else {
-            url = "https://ca.perfectomobile.com/nexperience/perfectomobile/wd/hub";
-            //for perfecto this should be a cloud device name like '00ABE6022E2931C6'
-            deviceName = argv.deviceName;
-            app = argv.app;
+function multiCapFunction() {
+    constructCapObject(Dlist.Emulator);
+    return multiCapabilities;
+};
+
+//manage device farm specific properties with cmd argument and if conditions
+function constructCapObject(list) {
+    var capabilitiesObj = {};
+    for(var i = 0; i < list.length; i++) {
+        capabilitiesObj["platformVersion"] = list[i].platformVersion;
+        capabilitiesObj["autoWebview"] = true;
+        capabilitiesObj["platformName"] = list[i].platformName;
+        capabilitiesObj["app"] = "c:\\sw\\app-debug.apk";
+        capabilitiesObj["browserName"] = "";
+        capabilitiesObj["fullReset"] = true;
+        capabilitiesObj["chromedriverExecutableDir"] = "C:\\sw\\ionic\\Drivers";
+        capabilitiesObj["deviceName"] = list[i].deviceName;
+        multiCapabilities.push(capabilitiesObj);
+        capabilitiesObj = {};
+    }
+};
+
+function takeScreenshot() {
+    return new Promise(function(resolve, reject) {
+        var filename = "adbtakescreenshot.png";
+        // temp location
+        var screenshotPath = __dirname + '/target';
+        var exec = require('child_process').exec;
+        // Take a screenshot and store it on the internal storage.
+        exec('adb shell /system/bin/screencap -p /sdcard/' + filename );
+
+        // Wait until the screenshot is created on android
+        var date = new Date();
+        var curDate = null;
+        do {
+            curDate = new Date();
         }
-    }
-} else {
-    if(argv.app) {
-        app = argv.app;
-        console.log("== Taking app from location: " + app + " ==");
-    } else {
-        console.log("== Using the default app location : " + app + " ==");
-        console.log("== USAGE: --app 'c:\\sw\\app.apk' ==");
-    }
-    if(argv.deviceName) {
-        console.log("== Using deviceName : " + deviceName + " ==");
-    } else {
-        console.log("== Using the default app location : " + deviceName + " ==");
-        console.log("== USAGE: --deviceName 'my phone' ==");
-    }
-}
+        while (curDate - date < 1000);
 
-if(argv.platformVersion) {
-    platformVersion = argv.platformVersion;
-} else {
-    console.log("== Using default version: " + platformVersion + " ==");
-    console.info('== Usage: --platformVersion 8.1.1 ==');
+        var command = 'adb pull /sdcard/'+ filename + ' ' + screenshotPath;
+        // Copy the screenshot from the internal storage to the local computer.
+        exec(command, function (err, stdout, stderr) {
+            if(err){
+                console.error('The screenshot could not be copied from the ANDROID device: ' + stdout + ' - to the dir: ' + __dirname);
+                console.log('The screenshot is stored on the ANDROID device with the name: ' + filename);
+                reject(err);
+            }
+            //resolve the promise
+            resolve(base64_encode(screenshotPath + '/' + filename));
+        });
+    });
+};
+
+//method to convert file into bitmap - returns a base64 encoded string of the image
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
 }
 
 exports.config = {
     seleniumAddress : url,
     framework: "jasmine",
-    getPageTimeout: 30000,    // increase timeout alloted for page load, fixes issues with deep link logins in Firefox
+    getPageTimeout: 30000,
 
-    // Capabilities to be passed to the webdriver instance.
-    "multiCapabilities": [{
-        "browserName": '',
-        "appiumVersion" : "1.8.1",
-        "platformName" : "Android",
-        "platformVersion": "8.1.0",
-        "app" : "c:\\sw\\app-debug.apk",
-        "deviceName": "emulator-5556",
-        "autoWebview" : true,
-        "chromedriverExecutableDir":"C:\\sw\\ionic\\Drivers"
-    },
-    {
-        "chromedriverExecutableDir":"C:\\sw\\ionic\\Drivers",
-        "browserName": '',
-        "appiumVersion" : "1.8.1",
-        "platformName" : "Android",
-        "platformVersion": "7.1.1",
-        "app" : "c:\\sw\\app-debug.apk",
-        "deviceName": "emulator-5554",
-        "autoWebview" : true
-    }],
-
+    getMultiCapabilities : multiCapFunction,
 
     specs:  ['firstSpec.js'],
     jasmineNodeOpts: {
         defaultTimeoutInterval: 60000
+    },
+    onPrepare: function() {
+         // For Generating allure results
+         jasmine.getEnv().addReporter(new allureReporter( {
+            resultsDir: 'target/e2e/allure-results'
+        }));
+
+        jasmine.getEnv().afterEach(function(done) {
+                takeScreenshot().then(function (png) {
+                    allure.createAttachment('Screenshot', function () {
+                        return new Buffer(png, 'base64');
+                    }, 'image/png')();
+                    done();
+                });
+        });
+
+        !fs.existsSync(__dirname + '/target') &&  fs.mkdirSync(__dirname + '/target');
+        !fs.existsSync(__dirname + '/target/e2e') &&  fs.mkdirSync(__dirname + '/target/e2e');
+        var reporterPath = path.resolve(path.join(__dirname, '/target/e2e'));
+        console.info('The E2E reports will be stored in:', reporterPath);
+
+        // add test line level console output
+        jasmine.getEnv().addReporter(new specReporter({
+            displayStacktrace: 'all'
+        }));
+
+        // needed for junit reporter in jenkins
+        jasmine.getEnv().addReporter(new reporters.JUnitXmlReporter({
+            'savePath': reporterPath,
+            'consolidate': true,
+            'consolidateAll': true
+        }));
+
+        jasmine.getEnv().addReporter(new jasmine2HtmlReporter({
+            savePath: './target/e2e/html/reports/',
+            takeScreenshots: false
+        }));
     }
 };
+
--- protractor.browser.conf.js
+++ protractor.browser.conf.js
@@ -1,43 +1,68 @@
 'use strict';
 
-var basePath = './src/test/system/common';
+var basePath = './src/test/system/common',
+    fs = require('fs'),
+    path = require('path'),
+    reporters = require('jasmine-reporters'),
+    specReporter = require('jasmine-spec-reporter').SpecReporter,
+    allureReporter = require('jasmine-allure-reporter'),
+    jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
     exports.config = {
-
         framework: "jasmine",
-
         directConnect: true,
-
         allScriptsTimeout: 30000,    // increase timeout alloted for page load, fixes issues with deep link logins in Firefox
-
         capabilities: {
-
             "browserName": 'chrome'
-
-  },
-
-        baseUrl: "http://localhost:8100",
-
-        specs:  ['./firstSpec.js'],
-
-        jasmineNodeOpts: {
-
-            defaultTimeoutInterval: 60000
-
         },
 
+        baseUrl: "http://localhost:8100",
+        specs:  ['./firstSpec.js'],
+        jasmineNodeOpts: {
+            defaultTimeoutInterval: 60000
+        },
         useAllAngular2AppRoots: true, 
 
         onPrepare: function() {
+            // For Generating allure results
+            jasmine.getEnv().addReporter(new allureReporter( {
+                resultsDir: 'target/e2e/allure-results'
+            }));
 
+            jasmine.getEnv().afterEach(function(done) {
+                browser.takeScreenshot().then(function (png) {
+                    allure.createAttachment('Screenshot', function () {
+                        return new Buffer(png, 'base64');
+                    }, 'image/png')();
+                    done(); 
+                });
+            });
             browser.get('');
-
- 
-
             //Maximize the browser before executing scripts
-
             browser.driver.manage().window().maximize();
 
-        }
+            !fs.existsSync(__dirname + '/target') &&  fs.mkdirSync(__dirname + '/target');
+            !fs.existsSync(__dirname + '/target/e2e') &&  fs.mkdirSync(__dirname + '/target/e2e');
+            var reporterPath = path.resolve(path.join(__dirname, '/target/e2e'));
+            console.info('The E2E reports will be stored in:', reporterPath);
+    
+            // add test line level console output
+            jasmine.getEnv().addReporter(new specReporter({
+                displayStacktrace: 'all'
+            }));
+    
+            // needed for junit reporter in jenkins
+            jasmine.getEnv().addReporter(new reporters.JUnitXmlReporter({
+                'savePath': reporterPath,
+                'consolidate': true,
+                'consolidateAll': true
+            }));
+    
+            jasmine.getEnv().addReporter(new jasmine2HtmlReporter({
+                savePath: './target/e2e/html/reports/',
+                screenshotsFolder: 'images',
+                takeScreenshotsOnlyOnFailures: true
+            }));
 
+        }
     };
