573c8b4b885e7ed8e34b19f7bb54b4f5b453b428
Santosh Panchali <saravanaraguram@outlook.com>
2018-07-17 17:37:49 +0530

    new commits with browserstack

--- appium.perfecto.conf.js
+++ appium.perfecto.conf.js
@@ -2,8 +2,8 @@
 var basePath = './src/test/system/common',
     argv = require('minimist')(process.argv.slice(2));
 // const host = 'ca.perfectomobile.com';
-const user = 'rsssa02@ca.com';
-const pass = 'P@ssw0rd';
+const user = 'user';
+const pass = 'pwd';
 
 exports.config = {
     "seleniumAddress" : "https://ca.perfectomobile.com/nexperience/perfectomobile/wd/hub",
@@ -30,5 +30,51 @@ exports.config = {
     specs:  ['firstSpec.js'],
     jasmineNodeOpts: {
         defaultTimeoutInterval: 60000
+    },
+    onPrepare: function () {
+        // Get access to the current configuration object
+        browser.getProcessedConfig()
+            .then(function (config) {
+                // Add the needed capabilites to the browser-object so we
+                // can access them when needed
+                browser.appPackage = config.capabilities.appPackage;
+                browser.bundleId = config.capabilities.bundleId;
+                browser.deviceProperties = config.capabilities.deviceProperties;
+                browser.platformname = config.capabilities.platformName;
+
+                var wd = require('wd'),
+                    protractor = require('protractor'),
+                    wdBridge = require('wd-bridge')(protractor, wd);
+                wdBridge.initFromProtractor(config);
+    
+                // Define some extra properties
+                var iOSProperties = {'identifier': browser.bundleId},
+                    androidProperties = {'name': 'Test.apk', 'identifier': 'io.ionic.starter'},
+                    params = browser.platformname.toLowerCase() === 'android' ? androidProperties : iOSProperties;
+    
+                // Now (version 7.0.2) there is a little problem with supporting
+                // the first async call Protractor does to set the default
+                // timeout (waitForAngular). This will be in januari / februari
+                // 2016. One of the workarounds it to disable the default
+                // waitForAngular with this line of code. Remove it when the
+                // the issue is fixe
+                browser.ignoreSynchronization = true;
+    
+                // Close the app
+                return browser.driver.executeScript('mobile:application:close', params)
+                    .then(function () {
+                        // start the app
+                        return browser.driver.executeScript('mobile:application:open', params);
+                    })
+                    .then(function () {
+                        // Switch to the webview 
+                        return wdBrowser.context('WEBVIEW');
+                    })
+                    .then(function () {
+                        // Added a sleep here to make sure the webview is loaded
+                        // this could be done smarter, but that's not part of this
+                        // scope 
+                        return browser.sleep(5000);});      
+                    });
     }
 };
---
+++ browserstack.protractor.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+exports.config = {
+    'seleniumAddress': 'http://hub-cloud.badarowserstack.com/wd/hub',
+    "framework": "jasmine",
+    "getPageTimeout": 30000,    // increase timeout alloted for page load, fixes issues with deep link logins in Firefox
+
+    // Capabilities to be passed to the webdriver instance.
+    "multiCapabilities": [{
+        "browserName": "",
+        'browserstack.user': 'user',
+        'browserstack.key': 'key',
+        'device': 'Google Nexus 6',
+        'realMobile': 'true',
+        'os_version': '6.0',
+        'browserstack.appium_version': '1.7.2',
+        'app': 'bs://d88d33add01f6d3534e83287299b62027f13cbbd',
+        'autoWebview': true,
+        'autoWebviewTimeout': 20000
+    },
+    {
+        "browserName": "",
+        'browserstack.user': 'saravanaraguram1',
+        'browserstack.key': '4zXinEeQnsxJpqTtPvur',
+        'device': 'Google Pixel',
+        'realMobile': 'true',
+        'os_version': '7.1',
+        'browserstack.appium_version': '1.7.2',
+        'app': 'bs://d88d33add01f6d3534e83287299b62027f13cbbd',
+        'autoWebview': true,
+        'autoWebviewTimeout': 20000
+    }
+    ],
+
+    specs:  ['firstSpec.js']
+};
